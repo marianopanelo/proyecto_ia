5af7db9bc23cab1fa40174e36e652440e304a0fc
Panelo Mariano Andres <marianoapanelo@gmail.com>
2026-09-02 12:32:35 -0300

    mapa
    Cree el mapa

--- Assets/script/Map.cs
+++ Assets/script/Map.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static Map instance {  get; private set; }
+
+    [SerializeField] private float width = 30f;
+    [SerializeField] private float height = 30f;
+    [SerializeField] private bool drawgizmos = true;
+
+    private void Awake()
     {
-        
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 OutOfMap(Vector3 position)
     {
-        
+        Vector3 newPosition = Vector3.zero;
+        if (position.x > width / 2f) newPosition.x = -width / 2f;
+        if (position.x < -width / 2f) newPosition.x = width / 2f;
+        if (position.z > height / 2f) newPosition.z = -height / 2f;
+        if (position.z < -height / 2f) newPosition.z = height / 2f;
+        return newPosition;
     }
+
+    private void OnDrawGizmos()
+    {
+        if (!drawgizmos) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(width, 0f, height));
+    }
+
+
+
 }
